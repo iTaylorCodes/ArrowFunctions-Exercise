28196b84a0297f870ea2e9d96f1698e3a973be93
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-04-02 20:45:02 -0700

    Completed Exercise

---
+++ Arrow_Functions_Exercise.js
@@ -0,0 +1,12 @@
+// ES5 Map Callback
+function double(arr) {
+	return arr.map(function (val) {
+		return val * 2;
+	});
+}
+
+// ES2015 Arrow Functions Shorthand
+const double = (arr) => arr.map((val) => val * 2);
+
+// Refactored function using arrow functions:
+const squareAndFindEvens = (numbers) => numbers.map((num) => num ** 2).filter((square) => square % 2 === 0);
